a9a7d9fa49210c4b406b51b41965eb35a4c43031
Kuba Górczyński <jacobemaci@gmail.com>
2017-03-31 01:46:36 +0200

    Exercises including cesar code

---
+++ L5 PD2 zadanie 3.py
@@ -0,0 +1,16 @@
+#zadanie 3
+alfabet1 = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ'
+alfabet2 = 'aąbcćdeęfghijklłmnńoóprsśtuwyzźż'
+
+def cezar(slowo,klucz):
+  alfabet = alfabet1 + alfabet2
+  regula = str.maketrans(alfabet,alfabet[klucz:] + alfabet[:klucz])
+  print (slowo.translate(regula), end='')  
+
+def szyfruj_napis(napis, klucz):
+  for x in napis:
+    if x in alfabet1 or alfabet2:
+      cezar(x, klucz)
+    else:
+      print(x, end='')
+      
