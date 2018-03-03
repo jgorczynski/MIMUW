afdcc3712f8e3f1614d762375955af14a29a5959
Kuba Górczyński <jacobemaci@gmail.com>
2018-03-03 15:06:27 +0100

    Mar 3, 2018 .py files rename

---
+++ L5_PD1_task1.py
@@ -0,0 +1,8 @@
+#zadanie 1
+for x in range(10, 100):
+    if(x % 7 == 0):
+        print(x, end=' ')
+
+#wzorzec
+for x in range(14, 100, 7):
+    print(x, end = ' ')
---
+++ L5_PD1_task2.py
@@ -0,0 +1,4 @@
+lista = ['Interdyscyplinarne', 'Centrum', 'Modelowania']
+for x in lista:
+    print(x[1] + "-" + x[-1] + " (" + str(len(x)) + ")")
+print()
