17c270672145ca670f38557949f44016e58e2e60
Kuba Górczyński <jacobemaci@gmail.com>
2018-04-18 23:38:47 +0200

    Apr 18, 2018 .py files update no. 3

--- 9th edition/stuff.py
+++ 9th edition/stuff.py
@@ -16,4 +16,37 @@ del(l[5])
 s = "".join(l)
 print(s)
 
+#defining var type
+a,b,c = 1, 3.14, 'Python'
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+c = (a == 1)
+print(c, type(c))
+
+#objectivity
+l = ['i', 'm']
+l.insert(1, 'c')
+print(l)
+l.reverse()
+print(l)
+l.sort()
+print(l)
+
+#6.4 dictionaries - key-value pairs
+
+slownik = {"bd" : "xx", 5 : True, "a" : 11}
+for klucz in slownik: 
+    print(klucz, "=>", slownik[klucz])
+
+if "bd" in slownik:
+    print("jest element o kluczu 'bd'")
+    del slownik['bd']
+slownik[15] = True
+slownik['a'] = 'yy'
+#for k, v in m.items():
+    #print (k, "=>", v)
+
+
+
 
