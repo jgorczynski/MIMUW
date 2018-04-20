faeb2518ea349c4d450c9788ddc64db1ca3d77f4
Kuba Górczyński <jacobemaci@gmail.com>
2018-04-20 10:58:43 +0200

    Apr 20, 2018 .py files update no. 1

--- 9th edition/pd1/task3.py
+++ 9th edition/pd1/task3.py
@@ -12,3 +12,16 @@ def two(a):
 l = ['Ala', 'ma', 'kota', 'i PSA']
 
 two(l)
+
+#or with str.replace()
+
+def two_replace(a):
+    b = 'aąbcćdeęfghijklłmnńoóprsśtuwyzźż'
+    for x in a:
+        x1=x #that's the one that was missing
+        for y in x:
+            if y in b:
+                x1 = x1.replace(y, 2*y)
+        print(x1)
+
+two_replace(l)
