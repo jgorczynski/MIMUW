bf4eb7ea0eab263b08dc937f262ce2fc40a0dafe
Kuba Górczyński <jacobemaci@gmail.com>
2018-04-29 19:50:02 +0200

    Files moved to ../Introduction | Apr 29, 2018

--- 9th edition/task1.py
+++
@@ -1,5 +0,0 @@
-#find 10-digits powers of two
-
-powers = [2**30, 2**31, 2**32, 2**33]
-
-print(powers)
--- 9th edition/task2.py
+++
@@ -1,6 +0,0 @@
-#find sum of 1**2 + 2**2 + ... + 99**2 + 100**2
-
-suma = 0
-for x in range(1, 101):
-    suma += x**2
-print(suma)
